42850030de83f7cdc7527dc16c8b7ee5666ba66f
Gautam Chaudhary <gkcgautam@gmail.com>
2019-06-24 18:41:45 +0530

    Create app style theme

---
+++ src/styles/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+    colors: {
+      base: '#000000',
+      accent: '#FF3B3F',
+      darkGrey: '#494D50',
+      brand: {
+        upraised: '#FF3B3F',
+      },
+    },
+  };
+  
+  export default theme;
