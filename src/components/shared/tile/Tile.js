dbb6afe1a827ee8f4e841b22b6db94ff024b256d
Gautam Chaudhary <gkcgautam@gmail.com>
2019-06-24 18:43:14 +0530

    Created shared Tile component

---
+++ src/components/shared/tile/Tile.js
@@ -0,0 +1,16 @@
+import styled from '@emotion/native';
+
+const Tile = styled.View({
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: {
+        width: 0,
+        height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    backgroundColor: '#fff',
+    elevation: 3,
+});
+
+export default Tile;
